bb8cb55c0509fd9389e59930dd3a5190ef658b2d
fide-developer <faadjarfirdaus@gmail.com>
2026-05-13 00:00:14 +0700

    feat: add welcome messages

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-import Image from "next/image";
+'use client'
+import { useRouter } from "next/navigation";
+import Button from "./components/Button";
 
 export default function Home() {
+  const router = useRouter()
+
   return (
-    <div>
-      User List
+    <div className="flex gap-10 flex-col w-full text-2xl items-center justify-center p-[20%]">
+      <span className="text-center">Hi There! Thank you for consider me as a candidate. I've done the test, hopefully we can continue to the next steps. Have a great time!</span>
+      <Button onClick={() => router.push('/users')} intent="primary" size="md" className="font-semibold">Go to Users Page</Button>
     </div>
   );
 }
