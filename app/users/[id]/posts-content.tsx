e8ef88f451614597ea163167bcf39cfc7d31b012
fide-developer <faadjarfirdaus@gmail.com>
2026-05-12 23:52:13 +0700

    feat: add error display and loader on the user details posts section

--- app/users/[id]/posts-content.tsx
+++ app/users/[id]/posts-content.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { UserPost } from "@/app/api/users/route"
+import Skeleton from "@/app/components/Skeleton"
 import useUserPosts from "@/app/hooks/useUsers/useUserPosts"
 
 interface PostContentCardProps {
@@ -23,6 +24,36 @@ const PostContentCard: React.FC<PostContentCardProps> = ({data}) => {
     )
 }
 
+const ErrorDisplay: React.FC = () => {
+    return (
+        <div className="flex w-full items-center justify-center">Opps, failed to load user's posts content. Please try again later.</div>
+    )
+}
+
+const UserPostLoader: React.FC = () => {
+    return (
+        <div>
+            {Array(5).fill(null).map((_, index) => (
+                <div
+                    key={index}
+                    className="flex flex-col flex-nowrap gap-4 mt-4 rounded-2xl
+                    border border-black/10
+                    bg-white/70 p-5
+                    shadow-sm backdrop-blur-sm
+                    dark:border-white/10
+                    dark:bg-white/3"
+                >
+                    <Skeleton className="h-8 w-[90%]" />
+                    <div className="flex flex-col gap-2">
+                        <Skeleton className="h-5 w-full" />
+                        <Skeleton className="h-5 w-[70%]" />
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+}
+
 export interface UserPostContentProps {
     userId: string
 }
@@ -30,6 +61,8 @@ export interface UserPostContentProps {
 export const UserPostContent: React.FC<UserPostContentProps> = ({ userId }) => {
     const {data: posts, isLoading, isError} = useUserPosts(userId)
 
+    if(isError) return <ErrorDisplay />
+    if(isLoading) return <UserPostLoader />
     return (
         <div>
             {posts?.map((post) => <PostContentCard key={post.id} data={post} />)}
